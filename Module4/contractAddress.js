c13c3a5377ed543c1bf0e941708a7d02b1e8ccda
Agbaje Opeyemi <damamy96@gmail.com>
2023-08-16 16:03:43 +0100

    Update contractAddress.js

--- Module4/contractAddress.js
+++ Module4/contractAddress.js
@@ -1,1 +1,2 @@
-const address = 0x201fd91A35c8a9638901C86A61316eBd7D532e22;
+const address = '0xF7c6e04392EBC6b13B31693b58f3BD99E0905439';
+const verificationLink = 'https://testnet.snowtrace.io/address/0xF7c6e04392EBC6b13B31693b58f3BD99E0905439#code';
